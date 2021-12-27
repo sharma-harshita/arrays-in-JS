ed5881dc1ee352ba77791671fcae16a04b94aad2
harshita.sharma <harshita.sharma@asksid.ai>
2021-12-27 20:27:44 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Hello");
+
+// Arrays : Array is a datatype in JS. It can store values of any data type
+
+let x = "Student";
+
+let user = [1, "John", 25000.00, "Software Developer", "helo", true];    //array 
+let employee = [1010, "Doe", "30K", "MERN Developer"]
+
+console.log(user, employee);
+
+
+// length of an array = to find out the number of elements present inside an array 
+
+const lenOfUser = user.length;
+const lenOfEmp = employee.length;
+
+console.log(lenOfUser, lenOfEmp);
+
+
+// how to access these values individually
+console.log("employee[1]",employee[1]);
+
+
+
+// ii.Arrays Methods - map, filter, reduce, index, push, pop, shift,  unshift, slice, some, every
+

82c367b1e778d873b8e4b2e5a5916984f7778f98
harshita.sharma <harshita.sharma@asksid.ai>
2021-12-27 20:44:04 +0530

    second

--- index.js
+++ index.js
@@ -2,26 +2,53 @@ console.log("Hello");
 
 // Arrays : Array is a datatype in JS. It can store values of any data type
 
-let x = "Student";
+// let x =
 
-let user = [1, "John", 25000.00, "Software Developer", "helo", true];    //array 
-let employee = [1010, "Doe", "30K", "MERN Developer"]
-
-console.log(user, employee);
 
 
 // length of an array = to find out the number of elements present inside an array 
 
-const lenOfUser = user.length;
-const lenOfEmp = employee.length;
+// const lenOfUser = user.length;
+// const lenOfEmp = employee.length;
 
-console.log(lenOfUser, lenOfEmp);
+// console.log(lenOfUser, lenOfEmp);
 
 
 // how to access these values individually
-console.log("employee[1]",employee[1]);
+// console.log("employee[1]",employee[1]);
 
 
 
-// ii.Arrays Methods - map, filter, reduce, index, push, pop, shift,  unshift, slice, some, every
+// ii.Arrays Methods - map, filter, reduce, index, push, pop, shift, unshift, slice, some, every
 
+// -------------------------------------------------------
+
+//push : helps you to add the new element at the end of the array
+
+let numbers = [5, 10, 15, 20, 25, 30]
+console.log("Before push operation",numbers);
+numbers.push(35);
+console.log("After push operation",numbers);
+
+// -------------------------------------------------------
+
+//pop : helps you to remove the last element from the array
+console.log("Before pop operation",numbers);
+numbers.pop();
+console.log("After pop operation",numbers);
+
+
+// -------------------------------------------------------
+
+//shift : helps you to remove the first element from the array
+console.log("Before shift operation",numbers);
+numbers.shift();
+console.log("After shift operation",numbers);
+
+
+// -------------------------------------------------------
+
+//unshift : helps you to add the element at the start of the array
+console.log("Before unshift operation",numbers);
+numbers.unshift(5);
+console.log("After unshift operation",numbers);

92233be96e38940f4718d43865ff7859ac793c33
harshita.sharma <harshita.sharma@asksid.ai>
2021-12-27 21:07:20 +0530

    commit

--- index.js
+++ index.js
@@ -25,30 +25,45 @@ console.log("Hello");
 
 //push : helps you to add the new element at the end of the array
 
-let numbers = [5, 10, 15, 20, 25, 30]
-console.log("Before push operation",numbers);
-numbers.push(35);
-console.log("After push operation",numbers);
+// let numbers = [5, 10, 15, 20, 25, 30]
+// console.log("Before push operation",numbers);
+// numbers.push(35);
+// console.log("After push operation",numbers);
 
 // -------------------------------------------------------
 
 //pop : helps you to remove the last element from the array
-console.log("Before pop operation",numbers);
-numbers.pop();
-console.log("After pop operation",numbers);
-
+// console.log("Before pop operation",numbers);
+// numbers.pop();
+// console.log("After pop operation",numbers);
 
 // -------------------------------------------------------
 
 //shift : helps you to remove the first element from the array
-console.log("Before shift operation",numbers);
-numbers.shift();
-console.log("After shift operation",numbers);
-
+// console.log("Before shift operation",numbers);
+// numbers.shift();
+// console.log("After shift operation",numbers);
 
 // -------------------------------------------------------
 
 //unshift : helps you to add the element at the start of the array
-console.log("Before unshift operation",numbers);
-numbers.unshift(5);
-console.log("After unshift operation",numbers);
+// console.log("Before unshift operation",numbers);
+// numbers.unshift(5);
+// console.log("After unshift operation",numbers);
+
+
+//LOOPS : 
+// in case of arrays loops basically helps you to iterate through each and every element.
+// map, reduce, filter
+
+
+//Question : double all the elements of the array
+let arr = [1, 2, 3, 4, 5]
+
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[index] * 2);
+// }
+
+// map function iterates through each element of the array, and do the required operation, then returns a new array.
+const newArr = arr.map( n => n * 2 );
+console.log(newArr);
